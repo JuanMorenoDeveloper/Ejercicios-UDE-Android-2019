5647f1892e964d729fbc77019cb8f7879c585617
Juan M.vi <earth001@gmail.com>
2019-05-13 00:51:25 -0300

    UPDATE Se agrega version inicial de practico 3

---
+++ practico03-intro-android/app/src/main/java/uy/edu/ude/MainActivity.kt
@@ -0,0 +1,12 @@
+package uy.edu.ude
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+    }
+}
