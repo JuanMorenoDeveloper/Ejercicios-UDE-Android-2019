d5cdf1463ac352a560c079f27f3beac36c3c194d
Juan M.vi <earth001@gmail.com>
2019-06-14 21:50:08 -0300

    Se agrega solucion a ejercicio 2 de practico 4

--- practico03-intro-android/app/src/main/java/uy/edu/ude/Calculadora.kt
+++ practico03-intro-android/app/src/main/java/uy/edu/ude/Calculadora.kt
@@ -5,4 +5,10 @@ class Calculadora {
     fun sum(n1: Int, n2: Int): Int {
         return n1 + n2
     }
+
+//    fun resta(n1: Int, n2: Int): Int {
+//        return n1 - n2
+//    }
+
+    fun resta(n1: Int, n2: Int) = n1 - n2
 }
--- practico03-intro-android/app/src/test/java/uy/edu/ude/CalculadoraUnitTest.kt
+++ practico03-intro-android/app/src/test/java/uy/edu/ude/CalculadoraUnitTest.kt
@@ -7,15 +7,30 @@ class CalculadoraUnitTest {
     // Con gradle se ejecutan con
     // gradle test
     @Test
-    fun givenTwoNumbers_whenSum_thenGetResult(){
-        //Setup
+    fun givenTwoNumbers_whenSum_thenGetResult() {
+        //Setup, Arrange
         val calc = Calculadora()
         val n1 = 5
         val n2 = 3
         val resultExpected = 8
 
         //SUT
-        val result = calc.sum(n1,n2)
+        val result = calc.sum(n1, n2)
+
+        //Assert
+        assertThat(result).isEqualTo(resultExpected)
+    }
+
+    @Test
+    fun givenTwoNumbers_whenResta_thenGetResult() {
+        //Arrange
+        val calc = Calculadora()
+        val n1 = 3
+        val n2 = 1
+        val resultExpected = 2
+
+        //SUT
+        val result = calc.resta(n1, n2)
 
         //Assert
         assertThat(result).isEqualTo(resultExpected)
